797ac7bbfdfbbb7951b5969656a0ee0b39c8e86b
Nathan Schwartz <nathan.schwartz95@gmail.com>
2015-09-23 22:49:01 -0500

    sessionStorage now stores data to be graphed

---
+++ Warshadow/scripts/arrayData.js
@@ -0,0 +1,28 @@
+define( function(){
+
+	function initializeArray(){
+		var myA = [];
+		for(var iter = 0; iter < 100; iter++){
+			myA.push([]);
+		}
+		for(var iters = 0; iters < 100; iters++){
+			myA[iters][0] = iters;
+		}
+		sessionStorage.setItem('array', JSON.stringify(myA));
+	};
+	
+	function getArray(){
+		var arr = sessionStorage.getItem('array');
+		return JSON.parse(arr);
+	};
+
+	function sendArray(myArray){
+		sessionStorage.setItem('array', JSON.stringify(myArray));
+	};
+	
+	return{
+		initializeArray : initializeArray,
+		getArray : getArray,
+		sendArray : sendArray
+	};
+});
