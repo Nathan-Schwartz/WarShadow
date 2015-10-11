7c0f4559beec43ccda4ec0ed0dc12bb6ba229097
Nathan Schwartz <nathan.schwartz95@gmail.com>
2015-10-11 04:20:55 -0500

    added counterjs

---
+++ Warshadow/scripts/assets/counters.js
@@ -0,0 +1,81 @@
+define(function(){
+// !!!! add reset to a hotkey
+
+	localStorage.setItem("kill", 0);
+	localStorage.setItem("headshot1" ,0);
+	localStorage.setItem("headshot2" ,0);
+	localStorage.setItem("headshot3" ,0);
+	localStorage.setItem("headshot4" ,0);
+	localStorage.setItem("headshot5" ,0);
+	
+	function resetCounters(){
+	localStorage.setItem("kill", 0);
+	localStorage.setItem("headshot1" ,0);
+	localStorage.setItem("headshot2" ,0);
+	localStorage.setItem("headshot3" ,0);
+	localStorage.setItem("headshot4" ,0);
+	localStorage.setItem("headshot5" ,0);
+	}
+	
+	function incrementKill(){
+		var kill = JSON.parse(localStorage.getItem("kill"));
+		kill++;
+		localStorage.setItem("kill", kill);
+	};
+	
+	function incrementHeadshot(num){ // !!!! switch statement?
+		if(num == 1){
+			var headshot1 = JSON.parse(localStorage.getItem("headshot1"));
+			headshot1++;
+			localStorage.setItem("headshot1", headshot1);
+		}else if(num == 2){
+			var headshot2 = JSON.parse(localStorage.getItem("headshot2"));
+			headshot2++;
+			localStorage.setItem("headshot2", headshot2);
+		}else if(num == 3){
+			var headshot3 = JSON.parse(localStorage.getItem("headshot3"));
+			headshot3++;
+			localStorage.setItem("headshot3", headshot3);
+		}else if(num == 4){
+			var headshot4 = JSON.parse(localStorage.getItem("headshot4"));
+			headshot4++;
+			localStorage.setItem("headshot4", headshot4);
+		}else if(num ==5){
+			var headshot5 = JSON.parse(localStorage.getItem("headshot5"));
+			headshot5++;
+			localStorage.setItem("headshot5", headshot5);
+		}else
+			console.log('invalide number sent to increment HS');
+	};
+	
+	
+	function getKill(){
+		return JSON.parse(localStorage.getItem("kill"));
+	};
+
+	function getHeadshot(num){
+		var val = 0;
+		
+		if(num == 1)
+			val = JSON.parse(localStorage.getItem("headshot1"));
+		else if(num == 2)
+			val = JSON.parse(localStorage.getItem("headshot2"));
+		else if(num == 3)
+			val = JSON.parse(localStorage.getItem("headshot3"));
+		else if(num == 4)
+			val = JSON.parse(localStorage.getItem("headshot4"));
+		else if(val == 5)
+			val = JSON.parse(localStorage.getItem("headshot5"));
+		else
+			console.log('invalide number sent to get HS');
+		return val;
+	};
+	
+	return{
+		resetCounters:resetCounters,
+		incrementKill:incrementKill,
+		incrementHeadshot:incrementHeadshot,
+		getKill:getKill,
+		getHeadshot:getHeadshot
+	};
+});
